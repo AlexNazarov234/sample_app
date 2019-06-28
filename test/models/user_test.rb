45491e2b608688857fef1b3ef523befd286ad5fe
Alex Nazarov <pokupka91@mail.ru>
2019-06-28 11:54:38 +0300

    add user validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,35 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@user = User.new(name: 'Serg', email: 'test@test.ru')
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "name should be present" do
+  	@user.name = "a" * 51
+  	assert_not @user.valid?
+  end
+
+  test "email should be present" do
+  	@user.email = "a" * 245 + 'tes@test.ru' 
+  	assert_not @user.valid?
+  end
+
+  test "email should have correct name" do
+  	@user.email = "testest.ru"
+  	assert_not @user.valid?
+  end
+
+  test "email should be unique" do
+  	duplicate_user = @user.dup
+  	duplicate_user.email = @user.email.upcase
+  	@user.save
+  	assert_not duplicate_user.valid?
+  end
+
 end
