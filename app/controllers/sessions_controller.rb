1a00770f4360b26a8b65dad5fac63b02eaeee938
Alex Nazarov <pokupka91@mail.ru>
2019-07-02 11:33:25 +0300

    add checkbox RememberMe in Login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		#осуществить вход и переадресовать на страницу профиля
   		log_in user
+      params[:session][:remember_me] =='1' ? remember(user) : forget(user)
   		redirect_to user
   	else
   		#создать сообщение об ошибке
@@ -16,5 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
--- test/integration/sity_layout_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SityLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
